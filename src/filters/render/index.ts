ba57933fe87c06ac3916c0a526cc1f4967fd7730
Mustafa Shabbir Masvi <mustafa.s.masvi@intimetec.com>
2022-12-30 10:40:54 +0530

    Add MattiasCRT and Sobel ShaderToy Effect

--- src/filters/render/index.ts
+++ src/filters/render/index.ts
@@ -2,10 +2,14 @@ import vignette from './vignette.json';
 import scanlines from './scanlines.json';
 import linocut from './linocut.json';
 import raindrop from './raindrop.json';
+import mattiasCrt from './mattiasCrt.json';
+import sobel from './sobel.json';
 
 export const filtersRender = [
     vignette, 
     scanlines, 
     linocut, 
-    raindrop
+    raindrop,
+    mattiasCrt, 
+    sobel,
 ];
